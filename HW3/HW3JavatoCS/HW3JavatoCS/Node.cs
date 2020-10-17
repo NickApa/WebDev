0eee0497da08261c10171b6277eb8350c6936f04
NickApa <napa15@mail.wou.edu>
2020-10-16 19:15:03 -0700

    Node Class finished

---
+++ HW3/HW3JavatoCS/HW3JavatoCS/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW3JavatoCS
+{
+    class Node<T>
+    {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
