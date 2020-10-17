1939470a996344bd788f230344998e0cbee7c073
NickApa <napa15@mail.wou.edu>
2020-10-17 16:37:19 -0700

    Finished the assignemnt, everything works!

--- HW3/HW3JavatoCS/HW3JavatoCS/Run.cs
+++ HW3/HW3JavatoCS/HW3JavatoCS/Run.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace HW3JavatoCS
@@ -10,12 +11,12 @@ namespace HW3JavatoCS
         private static void printUsage()
         {
             Console.WriteLine("Usage is \n" +
-            "tjava Main C inputfile outputfile\n\n" +
+            "dotnet run C inputfile outputfile\n\n" +
             "Where:" +
             "  C is the column number to fit to\n" +
             "  inputfile is the input text file \n" +
             "  outputfile is the new output file base name containing the wrapped text.\n" +
-            "e.g. java Main 72 myfile.txt myfile_wrapped.txt");
+            "e.g. dotnet run 72 myfile.txt myfile_wrapped.txt");
         }
 
         public static void Main(string[] args)
@@ -23,8 +24,8 @@ namespace HW3JavatoCS
             int C = 72;
             string inputFilename;
             string outputFilename = "output.txt";
-            var scanner = new StreamReader(new File());
-
+            StreamReader scanner;
+            IQueue<string> words = new LinkedQueue<string>();
 
             if(args.Length != 3)
             {
@@ -36,24 +37,87 @@ namespace HW3JavatoCS
                 C = Int32.Parse(args[0]);
                 inputFilename = args[1];
                 outputFilename = args[2];
-                var scanner = new StreamReader(inputFilename);
-                Console.WriteLine(scanner.ReadToEnd());
+                scanner = new StreamReader(inputFilename);
+                
+               
+                while(scanner.Peek() > 0)
+                {
+                    foreach(var word in scanner.ReadLine().Split(" "))
+                    {
+                        words.Push(word);
+                    }
+                }
+                
+                
+                
             }
-            catch(FileNotFoundException e)
+            catch(FileNotFoundException)
             {
                 Console.WriteLine("Could not find the input file");
                 Environment.Exit(1);
             }
-            catch(Exception e)
+            catch(Exception)
             {
                 Console.WriteLine("Something is wrong with the input");
                 printUsage();
                 Environment.Exit(1);
             }
 
-            IQueue<string> words = new LinkedQueue<string>();
-            words.Push(scanner)
+            int spaceRemaining = WrapSimply(words, C, outputFilename);
+            Console.WriteLine("Total spaces remaining (Greedy): " + spaceRemaining);  
 
+        }
+
+        private static int WrapSimply(IQueue<string> words, int columnLength, string outputFilename)
+        {
+            StreamWriter outy;
+
+            try
+            {
+                outy = new StreamWriter(outputFilename);
+
+                
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Cannot create or open " + outputFilename +
+                        " for writing.  Using standard output instead.");
+                outy = new StreamWriter(Console.OpenStandardOutput());
+                outy.AutoFlush = true;
+            }
+            int col = 1;
+            int spaceRemaining = 0;
+            while(!words.IsEmpty())
+            {
+                string str = words.Peek();
+                int length = str.Length;
+
+                if(col == 1)
+                {
+                    outy.Write(str);
+                    col += length;
+                    words.Pop();
+                }
+                else if((col + length) >= columnLength)
+                {
+                    outy.WriteLine();
+                    spaceRemaining += (columnLength - col) + 1;
+                    col = 1;
+                }
+                else
+                {
+                    outy.Write(" ");
+                    outy.Write(str);
+                    col += (length + 1);
+                    words.Pop();
+                }
+               
+                
+            }
+            outy.WriteLine();
+            outy.Flush();
+            outy.Close();
+            return spaceRemaining;
         }
     }
 }
