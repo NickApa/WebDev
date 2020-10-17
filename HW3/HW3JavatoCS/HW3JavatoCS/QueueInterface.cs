6d4fee03110cf08344f60484d8f0802c6c097930
NickApa <napa15@mail.wou.edu>
2020-10-16 20:14:02 -0700

    Fixed .gitignore, added other files from the hw3

---
+++ HW3/HW3JavatoCS/HW3JavatoCS/QueueInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW3JavatoCS
+{
+    interface QueueInterface<T>
+    {
+        T push(T element);
+
+        T pop();
+
+        T peek();
+
+        bool isEmpty();
+
+    }
+}
---
+++ HW3/HW3JavatoCS/HW3JavatoCS/QueueUnderFlowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+
+namespace HW3JavatoCS
+{
+    class QueueUnderFlowException : Exception
+    {
+        QueueUnderFlowException() : base() { }
+
+        QueueUnderFlowException(string message) : base(message) { }
+
+    }
+}
