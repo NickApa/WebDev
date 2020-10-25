b31a5317161729e07a26b669498e55d9f914cab8
NickApa <napa15@mail.wou.edu>
2020-10-24 19:09:46 -0700

    created homework 4 project and changed default format to match the assignment

---
+++ HW4/HW4Solution/HW4Project/Controllers/ColorInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HW4Project.Models;
+
+namespace HW4Project.Controllers
+{
+    public class ColorInt : Controller
+    {
+
+            private readonly ILogger<ColorInt> _logger;
+
+            public ColorInt(ILogger<ColorInt> logger)
+            {
+                _logger = logger;
+            }
+
+            public IActionResult Create()
+            {
+            _logger.LogInformation("This worked");
+
+            
+                return View();
+            }
+    }
+}
