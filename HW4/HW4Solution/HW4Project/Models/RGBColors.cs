74c28477e2d8a5d5318fd3f58da126116aef9400
NickApa <napa15@mail.wou.edu>
2020-10-24 19:50:43 -0700

    Finished RGB Color page

---
+++ HW4/HW4Solution/HW4Project/Models/RGBColors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace HW4Project.Models
+{
+    public class RGBColors
+    {
+        [Required]
+
+        [Range(0, 255)]
+
+        public int Red { get; set; }
+
+        [Range(0, 255)]
+
+        public int Green { get; set; }
+
+        [Range(0, 255)]
+
+        public int Blue { get; set; }
+
+        public string chosenColor()
+        {
+            Color colors = Color.FromArgb(Red, Green, Blue);
+            return ColorTranslator.ToHtml(colors);
+        }
+    }
+}
