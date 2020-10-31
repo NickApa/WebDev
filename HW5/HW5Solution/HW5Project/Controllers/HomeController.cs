144e6bc6d4fb0a122c872442b8c703ec3720281a
NickApa <napa15@mail.wou.edu>
2020-10-30 20:14:00 -0700

    started hw5, created assignment and updated preset files to work with assignment

---
+++ HW5/HW5Solution/HW5Project/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HW5Project.Models;
+using System.Linq.Expressions;
+
+namespace HW5Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpGet]
+        public IActionResult CurrentAssignments()
+        {
+            return View();
+        }
+
+        public IActionResult currentAssignments(Assignment assignment) 
+        {
+            if(ModelState.IsValid) 
+            {
+                return RedirectToAction();
+            }
+            return View(assignment);
+              
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
